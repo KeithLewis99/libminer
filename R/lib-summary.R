3f141a17ad4eb76b61a38d91189cdd0a61ad63f0
KeithLewis99 <Keith.Lewis@dfo-mpo.gc.ca>
2024-11-04 16:27:29 -0330

    Initial commit

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,3 +1,9 @@
+#' Library summaries
+#'
+#' @return
+#' @export
+#'
+#' @examples - lib_summary()
 lib_summary <- function() {
    pkgs <- utils::installed.packages()
    pkg_tbl <- table(pkgs[, "LibPath"])
